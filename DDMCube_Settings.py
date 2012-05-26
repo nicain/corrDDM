74bb250b7c4570a8fb429fecc7b00a0b59fd1fc7
Nick <nicain@uw.edu>
2012-05-26 16:09:40 +0000

    changes

--- DDMCube_Settings.py
+++ DDMCube_Settings.py
@@ -1,25 +1,25 @@
 # Define job:
-quickNamePrefix = 'corrDDMDebug'
+quickNamePrefix = 'corrDDMMIPSI'
 dryRun = 0
-localRun = 1
+localRun = 0
 runType = 'batch'			# 'batch' or 'wallTimeEstimate'
-waitForSims = 1
+waitForSims = 0
 wallTime = 5000
 wallTimeEstCount = 1
 queue = 'default'
 FD=0
 
 # Divide jobs among processing unit settings:
-nodes = 1
-procsPerNode = 1
+nodes = 6
+procsPerNode = 8
 repsPerProc = 1
-simsPerRep = 10000
+simsPerRep = 1040
 
 # Define job settings:
 settings={
-'theta':list(numpy.linspace(.01,30,2)),
-'N':[5],
-'dt':[.01],
+'theta':list(numpy.linspace(.01,1200,20)),
+'N':[240],
+'dt':[.1],
 'corr':[.15],
 'rP':[21],
 'rN':[19]}
