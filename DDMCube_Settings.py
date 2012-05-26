99a3ae6d3b03bead9107da3e6033ca7ebe70900b
Nick <nicain@uw.edu>
2012-05-26 18:10:06 +0000

    done

--- DDMCube_Settings.py
+++ DDMCube_Settings.py
@@ -1,9 +1,9 @@
 # Define job:
-quickNamePrefix = 'corrDDMSIPSPRTOvershootDebug'
+quickNamePrefix = 'corrDDMSIPSPRTOvershoot'
 dryRun = 0
-localRun = 1
+localRun = 0
 runType = 'batch'			# 'batch' or 'wallTimeEstimate'
-waitForSims = 1
+waitForSims = 0
 wallTime = 10000
 wallTimeEstCount = 1
 queue = 'default'
@@ -11,9 +11,9 @@ FD=0
 
 # Divide jobs among processing unit settings:
 nodes = 1
-procsPerNode = 1
+procsPerNode = 8
 repsPerProc = 1
-simsPerRep = 50
+simsPerRep = 6250
 
 Coh = 6.4
 rP = 40 + .4*Coh
@@ -21,7 +21,7 @@ rN = 40 - .4*Coh
 
 # Define job settings:
 settings={
-'theta':list(numpy.linspace(.01,3,20)),
+'theta':list(numpy.linspace(.01,6,20)),
 'N':[240],
 'dt':[.1],
 'corr':[.15],
