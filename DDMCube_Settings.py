caff8d235cd7b8f174cfeea8f69ea6c1a110e3f1
Nick <nicain@uw.edu>
2012-05-26 20:11:38 +0000

    done

--- DDMCube_Settings.py
+++ DDMCube_Settings.py
@@ -1,19 +1,19 @@
 # Define job:
-quickNamePrefix = 'corrDDMMIPSPRTDebug'
+quickNamePrefix = 'corrDDMMIPSPRT'
 dryRun = 0
-localRun = 1
+localRun = 0
 runType = 'batch'			# 'batch' or 'wallTimeEstimate'
-waitForSims = 1
+waitForSims = 0
 wallTime = 10000
 wallTimeEstCount = 1
 queue = 'default'
 FD=0
 
 # Divide jobs among processing unit settings:
-nodes = 1
-procsPerNode = 1
+nodes = 12
+procsPerNode = 8
 repsPerProc = 1
-simsPerRep = 100
+simsPerRep = 520
 
 Coh = 6.4
 rP = 40 + .4*Coh
@@ -21,7 +21,7 @@ rN = 40 - .4*Coh
 
 # Define job settings:
 settings={
-'theta':list(numpy.linspace(.01,3,10)),
+'theta':list(numpy.linspace(.01,3.5,20)),
 'N':[240],
 'dt':[.1],
 'corr':[.15],
