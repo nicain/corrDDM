ce25c0e37d1e785ca0f1e96c2ef1d43ce423b8dc
nicain <nicain@amath.washington.edu>
2012-05-27 20:33:05 +0000

    working

--- DDMCube_Settings.py
+++ DDMCube_Settings.py
@@ -1,28 +1,30 @@
 # Define job:
-quickNamePrefix = 'corrDDMSIPSPRTCorrCorrectAgain'
+quickNamePrefix = 'MatchedRTSIPSPRT'
 dryRun = 0
-localRun = 0
+localRun = 1
 runType = 'batch'			# 'batch' or 'wallTimeEstimate'
-waitForSims = 0
+waitForSims = 1
 wallTime = 10000
 wallTimeEstCount = 1
 queue = 'default'
 FD=0
 
 # Divide jobs among processing unit settings:
-nodes = 4
-procsPerNode = 8
+nodes = 1
+procsPerNode = 1
 repsPerProc = 1
-simsPerRep = 3125
+simsPerRep = 1000
 
 Coh = 6.4
 rP = 40 + .4*Coh
 rN = 40 - .4*Coh
 
+
+
 # Define job settings:
 settings={
-'theta':list(numpy.linspace(.01,6,20)),
-'N':[240],
+'theta':list(numpy.arange(0,2,numpy.log(rP/rN))),
+'N':[5],
 'dt':[.1],
 'corr':[.15],
 'rP':[rP],
